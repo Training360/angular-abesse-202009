d6d2b8c907edfa8d8229d340cb5b45250d1d5541
unknown <j.cserko@gmail.com>
2020-09-09 15:45:43 +0200

    Feat: async.

--- http/src/app/page/home/home.component.ts
+++ http/src/app/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, zip, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,18 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const i1 = interval(1000);
+    const i2 = interval(5000);
+    const i3 = interval(10000);
+
+    combineLatest([i1, i2, i3]).subscribe(
+      list => console.log(list)
+    );
+
+
+
+
+  }
 
 }
--- http/src/app/page/order/order.component.ts
+++ http/src/app/page/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from 'src/app/service/order.service';
 import { ConfigService } from 'src/app/service/config.service';
 import { Order } from 'src/app/model/order';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-order',
@@ -12,6 +13,7 @@ export class OrderComponent implements OnInit {
 
   cols = this.config.orderTable;
   list: Order[] = [];
+  list$: Observable<Order | Order[]> = this.orderService.read();
 
   constructor(
     private orderService: OrderService,
@@ -19,11 +21,11 @@ export class OrderComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.orderService.read().subscribe(
+    /* this.orderService.read().subscribe(
       orders => this.list = (orders as Order[]),
       error => console.error(error),
       () => console.log('completed')
-    );
+    ); */
   }
 
 }
