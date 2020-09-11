9f394efa249f3b389d58bf9f8072d7b1d592a2f8
unknown <j.cserko@gmail.com>
2020-09-11 12:01:39 +0200

    Feat: login page.

--- role/src/app/page/login/login.component.ts
+++ role/src/app/page/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  errorMessage = '';
+  loginData: User = new User();
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(ngform: NgForm): void {
+    this.auth.login(ngform.value).subscribe(
+      resp => this.router.navigate(['']),
+      err => this.errorMessage = err.message
+    );
   }
 
 }
